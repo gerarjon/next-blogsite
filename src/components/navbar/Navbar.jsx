19d26d8cd3767c86b3d729f3134150f303fe3e39
gerarjon <gerarsuaverdez@gmail.com>
2023-06-30 17:10:36 -0700

    added global styling and module css to footer and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
 
 const links = [
   {
@@ -37,16 +40,25 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">Blog Site</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>Blog Site</Link>
+      <div className={styles.links}>
         {links.map((link) => (
           <Link
             key={link.id}
-            href={link.url}>
+            href={link.url}
+            className={styles.link}>
             {link.title}
           </Link>
         ))}
+        <button
+          className={styles.logout}
+          onClick={()=>{
+            console.log("Logged Out")
+          }}
+          >
+          Logout
+        </button>
       </div>
     </div>
   )
