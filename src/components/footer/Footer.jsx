ce6d87428680c4d2f3c070c3fa9d8aec11bb2fa7
gerarjon <gerarsuaverdez@gmail.com>
2023-07-03 16:06:19 -0700

    added icons to footer and placeholder image for hero on homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>Footer</div>
+    <div className={styles.container}>
+      <div>2023 GerarSuaverdez. All rights reserved.</div>
+      <div>
+        <div className={styles.social}>
+          <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Gerar Suaverdez Facebook" />
+          <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Gerar Suaverdez Instagram" />
+          <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Gerar Suaverdez Twitter" />
+          <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Gerar Suaverdez" />
+        </div>
+      </div>
+    </div>
   )
 }
 
