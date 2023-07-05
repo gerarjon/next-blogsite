a15ddfeb85621817cedda9999a3f729826cea748
gerarjon <gerarsuaverdez@gmail.com>
2023-07-05 14:46:38 -0700

    added layout to 'about' page and created module for universal buttons

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/button/button'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image 
+          src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum unde iure sed culpa, recusandae nobis reprehenderit exercitationem, cupiditate quae saepe veniam, molestias soluta! Excepturi delectus perspiciatis quis recusandae! Beatae, possimus.
+          </p>
+        </div>
+        <div className={styles.item}>
+        <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum unde iure sed culpa, recusandae nobis reprehenderit exercitationem, cupiditate quae saepe veniam, molestias soluta! Excepturi delectus perspiciatis quis recusandae! Beatae, possimus.
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
+import Button from '@/components/button/button'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <p className={styles.desc}>
           Turning your idea into reality. We bring together the teams from the global tech industry.
         </p>
-        <button className={styles.button}>See Our Works</button>
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="" className={styles.img} />
