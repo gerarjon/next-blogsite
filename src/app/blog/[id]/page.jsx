55de027ed4a0c5ed6ac56f80c94d9f512e6837a4
gerarjon <gerarsuaverdez@gmail.com>
2023-07-06 13:31:43 -0700

    added layout/design for blog posts

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Lorem</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus error in blanditiis reiciendis autem similique provident veritatis, a suscipit veniam soluta odit magni quisquam ex expedita libero repellendus doloribus deserunt.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src=""
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt expedita corrupti inventore voluptas repellendus tempore. Earum, enim asperiores, placeat repudiandae quae aliquid delectus praesentium, laborum temporibus dolore possimus velit dolorem?
+        </p>
+      </div>
+    </div>
   )
 }
 
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import styles from './page.module.css'
 
 const Blog = () => {
   return (
     <div>
-      Blog
+      <Link href="/blog/testId" className={styles.mainContainer}>
+        <div className={styles.imageContainer}>
+          <Image 
+            src=""
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Description</p>
+        </div>
+      </Link>
     </div>
   )
 }
