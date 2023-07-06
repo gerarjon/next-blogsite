e773604519db30360ae9664f019609caba73205b
gerarjon <gerarsuaverdez@gmail.com>
2023-07-06 11:44:47 -0700

    added sample data and layout for portfolio/category section

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Button from '@/components/button/Button'
+import Image from 'next/image'
+import styles from './page.module.css'
+import { items } from './data.js'
 
-const Category = () => {
+const Category = ({params}) => {
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#"></Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.img}
+            src={items.image} 
+            alt="" 
+            fill="true" 
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from './page.module.css'
 
 const Portfolio = () => {
   return (
-    <div>
-      Portfolio
+    <div className={styles.container}>
+      <h1 className={styles.selectTtitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/applications" className={styles.item}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
     </div>
   )
 }
