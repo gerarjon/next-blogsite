c3ee29dc8d0707804ba59a71b8ae499cad9c06df
gerarjon <gerarsuaverdez@gmail.com>
2023-07-07 16:10:49 -0700

    testing client side fetching with swr and next

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: 'no-store',
+})
+  if (!res.ok) {
+    return notFound();
+  }
+  return res.json();
+}
+
+const BlogPost = async ({params}) => {
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Lorem</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus error in blanditiis reiciendis autem similique provident veritatis, a suscipit veniam soluta odit magni quisquam ex expedita libero repellendus doloribus deserunt.
           </p>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,26 +1,40 @@
+import { FETCH_CACHE_HEADER } from 'next/dist/client/components/app-router-headers'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import styles from './page.module.css'
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    cache: 'no-store',
+})
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
   return (
-    <div>
-      <Link href="/blog/testId" className={styles.mainContainer}>
-        <div className={styles.imageContainer}>
-          <Image 
-            src=""
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Description</p>
-        </div>
-      </Link>
+    <div className={styles.container}>
+      {data.map(item=>(
+        <Link href={`/blog/${item.id}`}  key={item.id} className={styles.mainContainer}>
+          <div className={styles.imageContainer}>
+            <Image 
+              src=""
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>Description</p>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
